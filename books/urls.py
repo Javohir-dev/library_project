450f9fa50f3553f84b7e6045a8b4319e149394eb
Javohir Khamidullaev <97449821+Javohir-dev@users.noreply.github.com>
2023-06-28 17:00:37 +0500

    DEtail, Delete and Update Views

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 
-from .views import BookListAPIView
+from .views import (
+    book_list_view,
+    BookListAPIView,
+    BookDetailAPIView,
+    BookDeleteAPIView,
+    BookUpdateAPIView,
+)
 
 urlpatterns = [
-    path("", BookListAPIView.as_view(), name="book_list"),
+    path("", BookListAPIView.as_view()),
+    path("<int:pk>/", BookDetailAPIView.as_view()),
+    path("<int:pk>/delete/", BookDeleteAPIView.as_view()),
+    path("<int:pk>/update/", BookUpdateAPIView.as_view()),
+    path("books/", book_list_view),
 ]
--- books/views.py
+++ books/views.py
@@ -1,9 +1,39 @@
 from django.shortcuts import render
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.generics import ListAPIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.generics import (
+    ListAPIView,
+    RetrieveAPIView,
+    DestroyAPIView,
+    UpdateAPIView,
+)
 
 
 class BookListAPIView(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+class BookDetailAPIView(RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDeleteAPIView(DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookUpdateAPIView(UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+@api_view(["GET"])
+def book_list_view(request, *args, **kwargs):
+    books = Book.objects.all()
+    serializer = BookSerializer(books, many=True)
+
+    return Response(serializer.data)
