fdad84ff5b4172428fb5d5dacb90f1e98727b71d
Javohir Khamidullaev <97449821+Javohir-dev@users.noreply.github.com>
2023-07-02 23:49:29 +0500

    swagger, redoc

--- books/urls.py
+++ books/urls.py
@@ -11,11 +11,11 @@ from .views import (
 )
 
 urlpatterns = [
-    path("", BookListAPIView.as_view()),
-    path("<int:pk>/", BookDetailAPIView.as_view()),
+    path("books/", BookListAPIView.as_view()),
+    path("books/<int:pk>/", BookDetailAPIView.as_view()),
     path("book-list-create/", BookCreateAPIView.as_view()),
     path("book/", BookListCreateAPIView.as_view()),
-    path("<int:pk>/delete/", BookDeleteAPIView.as_view()),
-    path("<int:pk>/update/", BookUpdateAPIView.as_view()),
-    path("<int:pk>/update-delete/", BookUpdateDeleteAPIView.as_view()),
+    path("books/<int:pk>/delete/", BookDeleteAPIView.as_view()),
+    path("books/<int:pk>/update/", BookUpdateAPIView.as_view()),
+    path("books/<int:pk>/update-delete/", BookUpdateDeleteAPIView.as_view()),
 ]
