ac146a2be2eb270be98c577bcb60285391b7dff3
Javohir Khamidullaev <97449821+Javohir-dev@users.noreply.github.com>
2023-07-05 23:53:32 +0500

    Added some Validations)

--- books/serializers.py
+++ books/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from .models import Book
 
 
@@ -9,3 +10,35 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ("id", "title", "content", "subtitle", "author", "isbn", "price")
+
+    def validate(self, data):
+        title = data.get("title", None)
+        isbn = data.get("isbn", None)
+
+        # Checking the title is not only a number!
+        if not title.isalpha():
+            raise ValidationError(
+                {
+                    "status": False,
+                    "books": "title must be str!",
+                }
+            )
+
+        # Checking isbn is not the same as others.
+        if Book.objects.filter(title=title, isbn=isbn).exists():
+            raise ValidationError(
+                {
+                    "status": False,
+                    "books": "The title and ISBN must be Unique!",
+                }
+            )
+        return data
+
+    # def validate_price(self, price):
+    #     if price < 0 or price > 9999999999:
+    #         raise ValidationError(
+    #             {
+    #                 "status": False,
+    #                 "message": "Wrong value of price!",
+    #             }
+    #         )
